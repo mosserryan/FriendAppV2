8424c936dccc4315e066b0c5847024bd40054c5c
mosserryan <mosser.ryan.a@gmail.com>
2024-04-29 21:06:57 -1000

    Initial commit for FriendAppV2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase //Takes text before word controller
+{
+    private readonly DataContext context;
+
+    public UsersController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await this.context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")] //api/users/id
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await this.context.Users.FindAsync(id);
+    }
+
+}
