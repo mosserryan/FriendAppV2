fcef0c92b183a614df1c489950730089bbca0b5c
mosserryan <mosser.ryan.a@gmail.com>
2024-05-05 20:43:39 -1000

    Created account controller with register endpoint and validation using DTO

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Controllers;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+
+    private readonly DataContext context;
+
+    public AccountController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpPost("register")] //POST: api/account/register
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+
+        if (await UserExists(registerDto.Username)) return BadRequest("Usnername is taken");
+
+        using var hmca = new HMACSHA512(); // "using" allows us to dispose of it manaully rather than waiting for GC to do it for us. Same as running hmca.Dispose() at the end.
+
+        var user = new AppUser 
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmca.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmca.Key
+        };
+
+        this.context.Users.Add(user);
+        await this.context.SaveChangesAsync();
+
+        return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await this.context.Users.AnyAsync(user => user.UserName == username.ToLower());
+    }
+
+}
